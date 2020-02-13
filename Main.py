2160595f13b76df1883c625e454454284235036c
平塚圭太 <g.h.py.2456@gmail.com>
2020-02-13 16:47:35 +0900

    edit

--- Main.py
+++ Main.py
@@ -6,7 +6,57 @@ import os
 import math
 import datetime
 import glob
-import random
+import random+
+import cv2
 
-model=tf.keras.applications.vgg16.VGG16()
-model.summary()
+import keras
+from keras.layers.convolutional import Conv2D,MaxPooling2D
+from keras.models import Sequential
+from keras.layers.core import Dense,Dropout,Activation,Flatten
+from keras.preprocessing.image import array_to_img,img_to_array,list_pictures,load_img,ImageDataGenerator
+from sklearn.model_selection import train_test_split
+
+img_wedth,img_vertical=224,224
+train_path = r'my_folder\train'
+test_path = r'my_folder\test'
+train_data=[]
+train_re=[]
+test_data=[]
+test_re=[]
+
+for img in list_pictures(train_path):
+    data=img_to_array(load_img(img,target_size=(img_wedth,img_vertical)))
+    train_data.append(data)
+    #train_re.append
+
+for img in list_pictures(test_path):
+    data=img_to_array(load_img(img,target_size=(img_wedth, img_vertical)))
+    test_data.append(data)
+
+train_data=np.asarray(train_data)
+test_data=np.asarray(test_data)
+
+model=Sequential()
+
+
+train_d_gen = ImageDataGenerator(
+    rescale=1/255
+)
+
+test_d_gen= ImageDataGenerator(
+    rescale=1/255
+)
+
+train_gene=train_d_gen.flow_from_directory(
+    train_path,
+    target_size=(img_wedth,img_vertical),
+    batch_size=32,
+    class_mode='sparse'
+)
+
+test_gene = test_d_gen.flow_from_directory(
+    test_path,
+    target_size=(img_wedth, img_vertical),
+    batch_size=32,
+    class_mode='sparse'
+)
